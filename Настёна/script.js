8c966ef738d49785e42e54371eac952f385c9fc9
Alexander Medvedev <86624245+ADDERALLZXC@users.noreply.github.com>
2022-03-08 00:52:59 +0300

    Add files via upload

---
+++ Настёна/script.js
@@ -0,0 +1,11 @@
+let images = new Array();
+
+let i;
+let nImages = 57;
+i = 1;
+for(i = 1;i<=nImages; i++) {
+	images[i] = 'images/' + "1 (" + i.toString() + ")"+ '.jpg';
+}
+
+console.log(i);
+i = 1;
